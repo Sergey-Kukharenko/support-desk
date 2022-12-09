018b99c3b3f2dde7c12f63eb597565033d5d31eb
Sergey Kukharenko <iamksergey@mail.ru>
2022-12-09 10:12:08 +0300

    init

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,9 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 } else {
   app.get('/', (_, res) => {
     res.status(200).json({message: 'Welcome to the Support Desk API'});
