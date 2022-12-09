270b3443be8bcf757346f98930c61711fa5c8a47
Sergey Kukharenko <iamksergey@mail.ru>
2022-12-09 10:18:05 +0300

    init

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,11 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, './frontend/build')))
 
   // FIX: below code fixes app crashing on refresh in deployment
   app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
   })
 } else {
   app.get('/', (_, res) => {
