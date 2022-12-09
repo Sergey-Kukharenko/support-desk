6af74ae1b9e3ab3c4d6a2f6f6f62d33bca3d93f8
Sergey Kukharenko <iamksergey@mail.ru>
2022-12-09 09:40:32 +0300

    init

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,35 @@
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import morgan from 'morgan'
-import { errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+const path = require('path');
+const express = require('express');
+require('colors');
+require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
 
+// Connect to database
+connectDB();
 
-dotenv.config()
+const app = express();
 
-connectDB()
-
-const app = express()
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
-
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
+// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
+  app.get('/', (_, res) => {
+    res.status(200).json({message: 'Welcome to the Support Desk API'});
+  });
 }
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
-
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port 5000 ${PORT}`.yellow.bold)
-)
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
